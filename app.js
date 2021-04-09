571fc41798c0ab6d1e307925332f721d671fd6e1
benl95 <beno_1995@hotmail.com>
2021-04-09 14:13:29 +0200

    feat: set up server and view engine

---
+++ app.js
@@ -0,0 +1,26 @@
+// Require modules
+const express = require('express');
+const hbs = require('express-handlebars');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const path = require('path');
+const port = 3000;
+
+// Routes
+const home = require('./routes/home');
+
+// Set view engine
+app.set('view engine', 'hbs')
+	.set('views', __dirname + '/views')
+	.engine('hbs', hbs({ extname: 'hbs', defaultLayout: 'main' }));
+
+// Set static directory and routes
+app.use(express.static(path.join(__dirname, '/public')))
+	.use(express.urlencoded({ extended: true }))
+	.use(home);
+
+// Initiate server on port
+server.listen(port, () => {
+	console.log(`Listening on port: ${port}`);
+});
