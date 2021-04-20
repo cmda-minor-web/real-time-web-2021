94ffbe3e62cb89cc1347dfb9220cfcb0deb0ccbe
benl95 <beno_1995@hotmail.com>
2021-04-20 12:39:36 +0200

    feat: create line chart, use dummy data

---
+++ public/js/script.js
@@ -0,0 +1,21 @@
+const labels = ['12:00 PM', '13:00 PM', '14:00 PM', '15:00 PM'];
+
+const data = {
+	labels: labels,
+	datasets: [
+		{
+			label: 'My first dataset',
+			backgroundColor: 'rgb(255, 99, 132)',
+			borderColor: 'rgb(255, 99, 132)',
+			data: [0, 10, 5, 2],
+		},
+	],
+};
+
+const config = {
+	type: 'line',
+	data,
+	options: {},
+};
+
+const lineChart = new Chart(document.querySelector('#lineChart'), config);
